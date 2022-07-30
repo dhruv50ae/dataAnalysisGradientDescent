1480fc2d1c8f3754034eceb9f6a04e68a7a37b47
dhruv50ae <linkdp2012@gmail.com>
2022-07-30 21:06:58 +0100

    Create graph with 2 variables

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
-# This is a sample Python script.
+# Imports and packages
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+import matplotlib.pyplot as plt
+import numpy as np
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+# A simple cost function
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+def f(x):
+    return x ** 2 + x + 1
+
+
+# Make Data
+
+x1 = np.linspace(start=-3, stop=3, num=500)
+
+# Plot
+
+plt.xlim([-3, 3])
+plt.ylim([0, 8])
+plt.xlabel("x1")
+plt.ylabel("f(x)")
+plt.plot(x1, f(x1))
+plt.show()
