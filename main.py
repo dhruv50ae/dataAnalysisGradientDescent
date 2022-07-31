5dec0214ccd3793c36c23c9cd2ac7376272ed238
dhruv50ae <linkdp2012@gmail.com>
2022-07-31 10:08:25 +0100

    Gradient Descent

--- main.py
+++ main.py
@@ -32,19 +32,64 @@ def df(x):
 
 
 # Plot function and derivative side by side
+#
+# plt.figure(figsize=[10, 5])
+#
+# # 1st Chart: Cost function
+#
+# plt.subplot(1, 2, 1)
+# plt.xlim([-3, 3])
+# plt.ylim([0, 8])
+# plt.xlabel("x1")
+# plt.ylabel("f(x)")
+# plt.plot(x1, f(x1))
+#
+# # 2nd Chart: Derivative
+#
+# plt.subplot(1, 2, 2)
+# plt.xlabel("x1")
+# plt.ylabel("df(x)")
+# plt.grid()
+# plt.xlim([-2, 3])
+# plt.ylim([-3, 6])
+# plt.plot(x1, df(x1))
+# plt.show()
+
+# Python Loops and Gradient Descent
+
+newX = 3
+previousX = 0
+stepMultiplier = 0.1
+precision = 0.0001
+
+xList = [newX]
+slopeList = [df(newX)]
+
+for n in range(200):
+    previousX = newX
+    gradient = df(previousX)
+    newX = previousX - stepMultiplier * gradient
+    stepSize = abs(newX - previousX)
+    xList.append(newX)
+    slopeList.append(df(newX))
+    if stepSize < precision:
+        break
+
+print(f"Local minimum is at {newX}, {df(newX)}, {f(newX)}")
+
+# Superimpose the gradient descent calculations on plot
 
 plt.figure(figsize=[10, 5])
-
-# 1st Chart: Cost function
 plt.subplot(1, 2, 1)
-
 plt.xlim([-3, 3])
 plt.ylim([0, 8])
 plt.xlabel("x1")
 plt.ylabel("f(x)")
 plt.plot(x1, f(x1))
 
-# 2nd Chart: Derivative
+values = np.array(xList)
+plt.scatter(xList, f(values), color="red", alpha=0.5)
+
 plt.subplot(1, 2, 2)
 plt.xlabel("x1")
 plt.ylabel("df(x)")
@@ -52,5 +97,7 @@ plt.grid()
 plt.xlim([-2, 3])
 plt.ylim([-3, 6])
 plt.plot(x1, df(x1))
+
+plt.scatter(xList, slopeList, color="red", alpha=0.5)
 
 plt.show()
